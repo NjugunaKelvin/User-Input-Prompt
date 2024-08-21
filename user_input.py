d4ce3745ec21e9d9d09d979572f904a98f93275b
NjugunaKelvin <njugunak349@gmail.com>
2024-08-21 15:36:31 +0300

    First commit

---
+++ user_input.py
@@ -0,0 +1,11 @@
+# get name
+name = input("What is your name? ")
+
+# get age
+age = input("How old are you? ")
+
+# get location
+location = input("Where do you live? ")
+
+# print
+print(f"Hello {name}, you are {age} years old and live in {location}.")
